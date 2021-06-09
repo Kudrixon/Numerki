6c8da6ec52b5d655d6e9e9f01a922e687e4e8f97
Kudrixon <35082391+Kudrixon@users.noreply.github.com>
2021-06-09 19:33:48 +0200

    Create euler.cpp

---
+++ euler.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+float func(float x, float y)
+{
+    return ((x-1)/(y*y));
+}
+
+void euler(float x0, float y, float h, float x)
+{
+    float temp = -0;
+
+    while (x0 < x) {
+        temp = y;
+        y = y + h * func(x0, y);
+        x0 = x0 + h;
+    }
+
+    cout << "Approximate solution at x = "
+        << x << "  is  " << y << endl;
+}
+
+int main()
+{
+    float x0 = 0;
+    float y0 = 1;
+    float h = 0.5;
+
+
+    float x = 1;
+
+    euler(x0, y0, h, x);
+    return 0;
+}
